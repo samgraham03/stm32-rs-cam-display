7aa4d973d8e23f4dcefb118412802cc9deeafe9d
Sam Graham <samagraham2003@gmail.com>
2025-04-02 23:21:28 -0400

    Add configuration logic for ST7735 display

--- src/main.rs
+++ src/main.rs
@@ -19,22 +19,114 @@ fn debug_print(usart2 : &stm32f401::USART2, s : &str) {
     }
 }
 
-fn setup_usart2(
+fn configure_peripherals(
     rcc : &stm32f401::RCC,
     gpioa : &stm32f401::GPIOA,
     usart2 : &stm32f401::USART2
 ) {
+
     // Enable GPIOA and USART2 clocks
     rcc.ahb1enr.modify(|_, w| w.gpioaen().enabled());
     rcc.apb1enr.modify(|_, w| w.usart2en().enabled());
 
-    // Set PA2 to USART2_TX alt function
-    gpioa.moder.modify(|_, w| w.moder2().alternate());
+    // Configure pin modes
+    gpioa.moder.modify(|_, w| {
+        w.moder0().output()
+         .moder1().output()
+         .moder2().alternate()
+         .moder4().output()
+    });
+
+    // Set PA2 to use USART2_TX
     gpioa.afrl.modify(|_, w| w.afrl2().af7());
+
+    /*
+        USART over USB
+
+        CON|PIN|NOTE
+        ==================
+        TX |PA2|USART2_TX
+    */
 
     // Set baud rate and enable USART2 Tx
     usart2.brr.write(|w| unsafe { w.bits(CLK_HZ/BAUD_RATE) });
     usart2.cr1.modify(|_, w| w.ue().enabled().te().enabled());
+
+    /*
+        ST7735 Display
+
+        CON|PIN|NOTE
+        ============
+        VCC|3.3|
+        GND|GND|
+        GND|GND|
+        NC |   |
+        NC |   |
+        NC |   |
+        CLK|PA5|SPI1_SCK
+        SDA|PA7|SPI1_MOSI
+        RS |PA4|Data/Command select (GPIO)
+        RST|PA1|Reset line (GPIO)
+        CS |PA0|Chip Select (GPIO)
+    */
+
+    // Disable Chip Select (CS)
+    gpioa.bsrr.write(|w| w.bs0().set_bit());
+
+    // Enable Reset (RST)
+    gpioa.bsrr.write(|w| w.br1().set_bit());
+
+    // ~120ms delay
+    asm::delay(CLK_HZ / 1000 * 120);
+
+    // Disable Reset (RST)
+    gpioa.bsrr.write(|w| w.bs1().set_bit());
+
+    // Put Register Select (RS) into command mode
+    gpioa.bsrr.write(|w| w.br4().set_bit());
+
+    // ~120ms delay
+    asm::delay(CLK_HZ / 1000 * 120);
+
+    /*
+        MicroSD card reader/writer
+
+        CON |PIN|NOTE
+        =============
+        CS  |
+        MOSI|
+        SCK |
+        MISO|
+    */
+
+    // TODO
+
+    /*
+        OV7670 Camera
+
+        CON |PIN|NOTE
+        =============
+        3.3V|
+        SCL |
+        VS  |
+        PLK |
+        D7  |
+        D5  |
+        D3  |
+        D1  |
+        RET |
+        DGND|
+        SDA |
+        HS  |
+        XLK |
+        D6  |
+        D4  |
+        D2  |
+        D0  |
+        PWDN|
+    */
+
+    // TODO
 }
 
 #[entry]
@@ -45,14 +137,12 @@ fn main() -> ! {
     let gpioa = &dp.GPIOA;
     let usart2 = &dp.USART2;
 
-    // Setup USART2 for dubug logging
-    setup_usart2(rcc, gpioa, usart2);
+    configure_peripherals(rcc, gpioa, usart2);
 
     loop {
         debug_print(usart2, "Hello World\r\n");
 
-        for _ in 0..1_000_000 {
-            asm::nop();
-        }
+        // ~1s delay
+        asm::delay(CLK_HZ);
     }
 }
