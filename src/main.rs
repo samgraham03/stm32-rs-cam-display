d2cfe644f2817baefcf09b032e4557acaa99d955
Sam Graham <samagraham2003@gmail.com>
2025-04-04 22:42:42 -0400

    Refactor peripheral configuration and setup SPI clock for st7735 display

--- src/main.rs
+++ src/main.rs
@@ -19,27 +19,11 @@ fn debug_print(usart2 : &stm32f401::USART2, s : &str) {
     }
 }
 
-fn configure_peripherals(
+fn configure_usart2_debugger(
     rcc : &stm32f401::RCC,
     gpioa : &stm32f401::GPIOA,
     usart2 : &stm32f401::USART2
 ) {
-
-    // Enable GPIOA and USART2 clocks
-    rcc.ahb1enr.modify(|_, w| w.gpioaen().enabled());
-    rcc.apb1enr.modify(|_, w| w.usart2en().enabled());
-
-    // Configure pin modes
-    gpioa.moder.modify(|_, w| {
-        w.moder0().output()
-         .moder1().output()
-         .moder2().alternate()
-         .moder4().output()
-    });
-
-    // Set PA2 to use USART2_TX
-    gpioa.afrl.modify(|_, w| w.afrl2().af7());
-
     /*
         USART over USB
 
@@ -48,10 +32,27 @@ fn configure_peripherals(
         TX |PA2|USART2_TX
     */
 
+    // Enable GPIOA clock
+    rcc.ahb1enr.modify(|_, w| w.gpioaen().enabled());
+
+    // Configure TX pin to use an alternate function
+    gpioa.moder.modify(|_, w| w.moder2().alternate());
+
+    // Set PA2 to use USART2_TX
+    gpioa.afrl.modify(|_, w| w.afrl2().af7());
+
+    // Enable USART2 clock
+    rcc.apb1enr.modify(|_, w| w.usart2en().enabled());
+
     // Set baud rate and enable USART2 Tx
     usart2.brr.write(|w| unsafe { w.bits(CLK_HZ/BAUD_RATE) });
     usart2.cr1.modify(|_, w| w.ue().enabled().te().enabled());
+}
 
+fn configure_st7735_display(
+    rcc : &stm32f401::RCC,
+    gpioa : &stm32f401::GPIOA
+) {
     /*
         ST7735 Display
 
@@ -69,6 +70,17 @@ fn configure_peripherals(
         RST|PA1|Reset line (GPIO)
         CS |PA0|Chip Select (GPIO)
     */
+
+    // Enable GPIOA clock
+    rcc.ahb1enr.modify(|_, w| w.gpioaen().enabled());
+
+    // Configure output pins
+    gpioa.moder.modify(|_, w| {
+        w.moder0().output() // CS
+         .moder1().output() // RST
+         .moder4().output() // RS
+         .moder5().alternate() // CLK
+    });
 
     // Disable Chip Select (CS)
     gpioa.bsrr.write(|w| w.bs0().set_bit());
@@ -88,6 +100,23 @@ fn configure_peripherals(
     // ~120ms delay
     asm::delay(CLK_HZ / 1000 * 120);
 
+    // Enable SPI1 clock
+    rcc.apb2enr.modify(|_, w| w.spi1en().enabled());
+
+    // Configure SPI pins
+    gpioa.moder.modify(|_, w| {
+        w.moder5().alternate() // CLK
+         .moder7().alternate() // SDA
+    });
+
+    // Set SPI pin alternate functions
+    gpioa.afrl.modify(|_, w| {
+        w.afrl7().af5() // SPI1_SCK
+         .afrl2().af7() // SPI1_MOSI
+    });
+}
+
+fn configure_microsd_interface() {
     /*
         MicroSD card reader/writer
 
@@ -100,7 +129,9 @@ fn configure_peripherals(
     */
 
     // TODO
+}
 
+fn configure_ov7670_camera() {
     /*
         OV7670 Camera
 
@@ -129,6 +160,20 @@ fn configure_peripherals(
     // TODO
 }
 
+fn configure_peripherals(
+    rcc : &stm32f401::RCC,
+    gpioa : &stm32f401::GPIOA,
+    usart2 : &stm32f401::USART2
+) {
+    configure_usart2_debugger(rcc, gpioa, usart2);
+
+    configure_st7735_display(rcc, gpioa);
+
+    configure_microsd_interface();
+
+    configure_ov7670_camera();
+}
+
 #[entry]
 fn main() -> ! {
     let dp = stm32f401::Peripherals::take().unwrap();
@@ -138,6 +183,8 @@ fn main() -> ! {
     let usart2 = &dp.USART2;
 
     configure_peripherals(rcc, gpioa, usart2);
+
+    display_hello_world();
 
     loop {
         debug_print(usart2, "Hello World\r\n");
